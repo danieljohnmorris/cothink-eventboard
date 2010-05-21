bf1368afccff70e5902657eb0e82784aabe6adaf
Daniel Morris <daniel.john.morris@googlemail.com>
2010-05-21 01:50:57 +0100

    add pending cuke steps

---
+++ features/step_definitions/eventboard_steps.rb
@@ -0,0 +1,19 @@
+When /^there are no events happening soon$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a message saying 'no upcoming events'$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a form for adding an event$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^there are events happening soon$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a list of upcoming events$/ do
+  pending # express the regexp above with the code you wish you had
+end
