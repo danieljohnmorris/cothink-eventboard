350676d643545dde27c36970f34f8c0819d06708
Tim Ruffles <timruffles@Tims-Laptop.local>
2010-05-25 17:21:33 +0100

    View spec for home started

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,8 +1,29 @@
 require 'spec_helper' 
 
 describe "home/index.html.erb" do 
-  it "displays a hello message" do 
-    render 
-    response.should contain("Hello!") 
-  end 
+  
+  context "when no events given" do
+    
+    before :each do
+      assigns[:events] = []
+    end
+    
+    it "should display a helpful message saying there were none found" do
+      render
+      p response
+      response.should contain("Opps, couldn't find any events. Try being less specific about the type of event.")
+    end
+  end
+  
+  context "given a list of events" do
+    
+    before :each do
+      assigns[:events] = [stub("Event"),stub("Event")]
+    end
+   
+    it "should render a list of events"
+    it "should render the events in a calendar format"
+    it "should display a booking link"
+  end
+  
 end 
