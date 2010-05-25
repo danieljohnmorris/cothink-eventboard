084d90373b245600f803a2ec0f17d518ca29c07f
Tim Ruffles <timruffles@Tims-Laptop.local>
2010-05-25 20:34:05 +0100

    Event passing all formatting tests, event list needs to use to pass its tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,27 @@
 class Event < ActiveRecord::Base
+  
   belongs_to :organisation
+  
+  def _formatted_date(date,piece)
+    date = self.send(date)
+    if piece == 'ampm'
+      date.strftime('%p').downcase
+    elsif piece == 'day'
+      date.day
+    elsif piece == 'month'
+      date.strftime('%b')
+    else
+      hour = date.strftime('%I').gsub(/^0/,'')
+      minutes = date.strftime('%M')
+      minutes == '00' ? hour : "#{hour}.#{minutes}"
+    end
+  end
+  
+  def method_missing(method,*args)
+    if method.to_s =~ /^(start|end)_(ampm|time|day|month)$/
+      _formatted_date($1 + '_date',$2)
+    else
+      super(method,*args)
+    end
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,31 +2,33 @@ require 'spec_helper'
 
 describe Event do
   
-  event_times = ['18-10-2010 19:00','18-10-2010 19:30','18-10-2010 19:06','12-10-2010 11:00','12-10-2010 12:00','12-10-2010 1:00']
-  event_expects = [[7,'pm',18],['7.30','pm',18],['7.06','pm',18],['11','am',12],['12','pm',12],['1','am',12]]
+  event_times = {'18-10-2010 19:00'=>['7','pm',18],
+                 '18-10-2010 19:30'=>['7.30','pm',18],
+                 '18-10-2010 19:06'=>['7.06','pm',18],
+                 '12-10-2010 11:00'=>['11','am',12],
+                 '12-10-2010 12:00'=>['12','pm',12],
+                 '12-10-2010 1:00'=>['1','am',12]}
   
   month = 'Oct'
   
-  indx = 0
-  event_times.each do |time|
-    time, ampm, day = event_expects[ indx ]
+  event_times.each_pair do |datetime, event_expects|
+    time, ampm, day = event_expects
     
-    event = Event.new(:start_time => time)
+    event = Event.new(:start_date => Time.parse(datetime))
     
     it "should present a nicely formatted start time" do
-      event.start_time_.should == time
+      event.start_time.should == time
     end
     it "should know if it has an am or a pm start" do
        event.start_ampm.should == ampm
      end
-    it "should present a nicely formatted start dau" do
-      event.formatted_start_date.should == day
+    it "should present a nicely formatted start day" do
+      event.start_day.should == day
     end
     it "should present a nicely formatted start month" do
-      event.formatted_start_date.should == month
+      event.start_month.should == month
     end
     
-    indx += 1
   end
   
 end
