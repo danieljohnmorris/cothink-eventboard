833a710f0aef3c9115e800b7e4ba5ed35ed00714
Tim Ruffles <timruffles@Tims-Laptop.local>
2010-05-26 17:47:37 +0100

    Moved truncate_first_sentence towards library quality, allowing for money, dates etc, and ?! in the sentence

--- vendor/plugins/cothink/lib/helpers/view.rb
+++ vendor/plugins/cothink/lib/helpers/view.rb
@@ -2,8 +2,16 @@ module Cothink
   module Helpers
     module View
       def truncate_first_sentence(str, max_chars, deliminator = nil)
-        # match first sentence
-        /^([^\.]*\.)/ =~ str
+        # match first sentence, defined as lots of none full stop characters
+        /^
+          ( 
+            (?:
+              [^\.] | # anything but a full stop, or...
+              \.(?=\d) # full stop followed by a number, eg .1, to allow dates 12.10.2010, money £12.90
+            )*
+            (?:\.|!|\?) # followed by a full stop, exclamation or question mark
+          )
+        /x =~ str
         return $1 if $1.respond_to?(:length) && $1.length < max_chars
         truncate(str,max_chars,deliminator)
       end
--- vendor/plugins/cothink/spec/view_helpers_spec.rb
+++ vendor/plugins/cothink/spec/view_helpers_spec.rb
@@ -13,7 +13,7 @@ describe Cothink::Helpers::View do
   end
   
   
-  it "should truncate a string to its first sentence if under max" do
+  it "should truncate a string to its first sentence if under max character limit" do
     testview.truncate_first_sentence("Hello, this is a short sentence. Extra stuff here",40).should == "Hello, this is a short sentence."
   end
   
@@ -21,4 +21,31 @@ describe Cothink::Helpers::View do
     
     testview.truncate_first_sentence("Hello, this is a short sentence. Extra stuff here", 20, '...').should == "called truncate"
   end
+  
+  it "should handle non sentances by passing off" do
+    testview.truncate_first_sentence("Hello, this is a short sentence Extra stuff here", 20, '...').should == "called truncate"
+  end
+  
+  it "should handle empty strings by passing off" do
+    testview.truncate_first_sentence("", 20, '...').should == "called truncate"
+  end
+  
+  it "should handle weird strings" do
+     testview.truncate_first_sentence("(H[]££££$$$30eloo). Moo", 50, '...').should == "(H[]££££$$$30eloo)."
+   end
+   
+   it "should intelligently handle times in the string" do
+      testview.truncate_first_sentence("See you at 19.00. Moo", 50, '...').should == "See you at 19.00."
+    end
+   it "should intelligently handle dates in the string" do
+        testview.truncate_first_sentence("See you on 12.09.11, ok. Moo", 50, '...').should == "See you on 12.09.11, ok."
+   end
+   it "should intelligently handle money in the string" do
+         testview.truncate_first_sentence("It'll cost you £19.00, cheap. Moo", 50, '...').should == "It'll cost you £19.00, cheap."
+  end
+  it "should work with exclamation and question marks too" do
+    testview.truncate_first_sentence("See you there? Moo", 50, '...').should == "See you there?"
+    testview.truncate_first_sentence("See you there! Moo", 50, '...').should == "See you there!"
+  end
+  
 end
