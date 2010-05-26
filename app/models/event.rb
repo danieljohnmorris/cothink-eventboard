f4e7baac8e745b288181c2dada2d355a3e05deda
Daniel Morris <daniel.john.morris@googlemail.com>
2010-05-26 14:25:07 +0100

    event mysql bugs - scale back, try to find working basic example

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,22 +4,26 @@ class Event < ActiveRecord::Base
   belongs_to :organisation
   
   def self::ingest_csv(csv_file)
-    events = []
+    count_saved = 0
+    e = Event.create(:title => "dan")
     
-    @parsed_file= FasterCSV.read("features/files/events_6_rows_over_11_lines.csv", :headers => true, :skip_blanks => true)
-    @parsed_file.each_with_index do |row, index| 
-      e = Event.new
-      events << e
-      e.title = row[0]
-      e.description = row[1]
-      e.start_date = Date.parse(row[2])
-      e.location = row[3]
-      e.source = row[4]
-      e.url = row[5]
-      e.organisation = Organisation.find_or_create_by_name(row[6])
-
-      puts e.inspect
-    end
+    # @parsed_file= FasterCSV.read("features/files/events_6_rows_over_11_lines.csv", :headers => true, :skip_blanks => true)
+    # @parsed_file.each_with_index do |row, index|
+    #   # e = Event.create(
+    #   #   :title => row[0]
+    #   # )
+    #   # e.description = row[1]
+    #   # e.start_date = Date.parse(row[2])
+    #   # e.location = row[3]
+    #   # e.source = row[4]
+    #   # e.url = row[5]
+    #   # e.organisation = Organisation.find_or_create_by_name(row[6])
+    #   # 
+    #   # count_saved += 1 if e.save
+    #   # e.save
+    # end
+    
+    count_saved
   end
 
   def _formatted_date(date,piece)
