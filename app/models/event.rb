c42bef976da4cb4aa77943f470196b28707d3eb0
Tim Ruffles <timruffles@Tims-Laptop.local>
2010-05-26 15:06:17 +0100

    REmoved overridden respond_to? method until i understand it :)

--- app/models/event.rb
+++ app/models/event.rb
@@ -24,7 +24,4 @@ class Event < ActiveRecord::Base
       super(method,*args,&block)
     end
   end
-  def respond_to?(sym)
-     sym =~ /^(start|end)_(ampm|time|day|month)$/ || super(sym)
-   end
 end
