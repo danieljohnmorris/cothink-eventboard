1685e431d4220c865cb0a702ac8211879d7ed3ee
Daniel Morris <dan@Belobog.local>
2010-06-10 15:15:22 +0100

    add rpx bits

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,9 @@
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "please-change-me@config-initializers-devise.com"
+  config.mailer_sender = "dan@thebusinessdiary.co.uk"
+
+  config.rpx_application_name = "the-business-diary" # The name of your RPX application (this is the name, NOT the API key!)
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default), :mongoid
