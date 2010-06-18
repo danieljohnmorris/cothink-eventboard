f04e7715ff14a0b29f675003656bdc8ee657439a
Daniel Morris <dan@Belobog.local>
2010-06-18 15:05:30 +0100

    halfway dan progress playing with stars!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+	$("a.starred").hover(function() {
+		$("img.star", this).attr("src", "/images/star.png");
+	}, function() {
+		$("img.star", this).attr("src","/images/star_dimmed.gif");
+	});
+});
