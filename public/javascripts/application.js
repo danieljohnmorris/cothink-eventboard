cea16048eef725a7ea9993602ac3ceb440b9bc2b
Daniel Morris <dan@Belobog.local>
2010-06-19 17:25:11 +0100

    bugfix: revert to using gif dimmed star

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,11 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function() {
+	
+	// Highlight star on hover!
 	$("a.star").hover(function() {
 		$("img", this).attr("src", "/images/star.png");
 	}, function() {
-		$("img", this).attr("src","/images/star_dim.png");
+		$("img", this).attr("src", "/images/star_dim.gif");
 	});
 });
