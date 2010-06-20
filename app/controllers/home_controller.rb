83f3dc04697ea51b4a943332948c24baf67a6e70
Daniel Morris <dan@Belobog.local>
2010-06-20 20:44:28 +0100

    Bugfix: events were stuck in may. Joined Event.find conditions into one string, seems to have done the trick!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     # annoying cant use named scope Event.published here! 
-    @events = Event.paginate :page => params[:page], :order => 'start_date ASC', :conditions => ["publish_state = #{Event::PUBLISHED_STATE}", 'start_date > NOW()']
+    @events = Event.paginate :page => params[:page], :order => 'start_date ASC', :conditions => ["publish_state = ? AND start_date >= NOW()", Event::PUBLISHED_STATE]
   end
 end
